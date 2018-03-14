371d288a18ab840783335999a7aa16a120fbfb2c
chenchongsong <chenchongsong@outlook.com>
2018-03-14 17:00:10 +0800

    fix type error to pass Travis Cl

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,9 +5,9 @@ import java.util.Set;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.money.Money;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
-import seedu.address.model.money.Money;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
