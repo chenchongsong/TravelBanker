e57a1de18517e97d3dbe1a22c8e4bbeae1d6910d
chenchongsong <chenchongsong@outlook.com>
2018-03-28 13:58:24 +0800

    support multiple target people

--- src/main/java/seedu/address/logic/commands/SplitCommand.java
+++ src/main/java/seedu/address/logic/commands/SplitCommand.java
@@ -15,6 +15,7 @@ import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -30,50 +31,55 @@ public class SplitCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "split";
     public static final String COMMAND_SHORTCUT = "sp";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Split a bill evenly among specified people "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Split a bill evenly among specified people. "
             + "Parameters: INDEX1 INDEX2 ... "
             + "[" + PREFIX_MONEY + "MONEY] ";
 
-    public static final String MESSAGE_SETTLE_PERSON_SUCCESS = "Settled Person: ";
+    public static final String MESSAGE_SPLIT_BILL_SUCCESS = "Bill Split: ";
 
-    private final Index index;
+    private final ArrayList<Index> indices;
 
-    private Person personToEdit;
-    private Person editedPerson;
+    private ArrayList<Person> peopleToEdit;
+    private ArrayList<Person> editedPeople;
 
     /**
-     * @param index of the person in the filtered person list to settle
+     * @param indices of people in the filtered person list to settle the bill
      */
-    public SplitCommand(Index index) {
-        requireNonNull(index);
-
-        this.index = index;
+    public SplitCommand(ArrayList<Index> indices, double bill) {
+        requireNonNull(indices);
+        this.indices = indices;
+        peopleToEdit = new ArrayList<>();
+        editedPeople = new ArrayList<>();
     }
 
     @Override
     public CommandResult executeUndoableCommand() throws CommandException {
         try {
-            model.updatePerson(personToEdit, editedPerson);
+            for (int i=0; i<indices.size(); i++) {
+                model.updatePerson(peopleToEdit.get(i), editedPeople.get(i));
+            }
         } catch (DuplicatePersonException dpe) {
             throw new AssertionError("The target people cannot be duplicate");
         } catch (PersonNotFoundException pnfe) {
             throw new AssertionError("The target people cannot be missing");
         }
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(index));
-        return new CommandResult(MESSAGE_SETTLE_PERSON_SUCCESS + editedPerson.getName());
+        // EventsCenter.getInstance().post(new JumpToListRequestEvent(index));
+        return new CommandResult(MESSAGE_SPLIT_BILL_SUCCESS);
     }
 
     @Override
     protected void preprocessUndoableCommand() throws CommandException {
         List<Person> lastShownList = model.getFilteredPersonList();
 
-        if (index.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        for (Index index : indices) {
+            if (index.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+            Person person = lastShownList.get(index.getZeroBased());
+            peopleToEdit.add(person);
+            editedPeople.add(getSettledPerson(person));
         }
-
-        personToEdit = lastShownList.get(index.getZeroBased());
-        editedPerson = getSettledPerson(personToEdit);
     }
 
 
--- src/main/java/seedu/address/logic/parser/SplitCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SplitCommandParser.java
@@ -5,26 +5,46 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.SplitCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MONEY;
 
 /**
- * Parses input arguments and creates a new SettleCommand object
+ * Parses input arguments and creates a new SplitCommand object
  */
 public class SplitCommandParser implements Parser<SplitCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the SettleCommand
-     * and returns a SettleCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the SplitCommand
+     * and returns a SplitCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public SplitCommand parse(String args) throws ParseException {
+
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_MONEY);
+
+        ArrayList<Index> indices;
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new SplitCommand(index);
+            indices = ParserUtil.parseIndices(argMultimap.getPreamble());
         } catch (IllegalValueException ive) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, SplitCommand.MESSAGE_USAGE));
         }
+
+        double bill;
+        try {
+            Optional<String> money = argMultimap.getValue(PREFIX_MONEY);
+            bill = Double.parseDouble(money.get());
+
+        } catch (Exception e) {
+            throw new ParseException("Exception Raised");
+        }
+
+        return new SplitCommand(indices, bill);
     }
 
 }
